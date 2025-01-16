d0a19020e70d3ece64d2fa35bd2ade9f3e813059
vasanthipl <vasanthi9822@gmail.com>
2025-01-16 14:57:00 +0530

    created offers

---
+++ src/test/java/com/swiggy/vasanthi.java
@@ -0,0 +1,32 @@
+package com.swiggy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class vasanthi {
+	
+	public static void main(String[] args)
+	
+	{
+		String n="Vasanthi";
+		char[]F=n.toCharArray();
+		
+	Map<Character,Integer> g = new HashMap<Character,Integer>();
+	for(Character x:F) {
+		if(g.containsKey(x))
+		{ 
+			Integer a=g.get(x);
+		g.put(x,a+1);
+		}
+		else
+		{
+			g.put(x,1);
+		}
+		}
+		
+		System.out.println(g);
+	}
+
+}
+
+
